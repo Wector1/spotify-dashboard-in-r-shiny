2e78eea6d06b9f2eb266f082210902cd07310b9b
addobosz <adamdobosz.poczta@gmail.com>
2024-05-28 22:21:03 +0200

    add animated plot and data transforming script

---
+++ src/transform_data.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(plotly)
+data <- read.csv("spotify_data/spotify_data.csv")
+
+# create new data and retain only the columns we need: year, 
+data_pruned <- data[, c("year", "popularity", "genre", "danceability")]
+
+# drop data from 2023 because it is incomplete
+data_pruned <- data_pruned %>% filter(year != 2023)
+
+# for each year and genre calculate the average popularity and danceability
+data_aggregated <- data_pruned %>%
+  group_by(year, genre) %>%
+  summarise(avg_popularity = mean(popularity), avg_danceability = mean(danceability), count = n())
+
+# export data_aggregated to a csv file
+write.csv(data_aggregated, "spotify_data/spotify_data_aggregated.csv", row.names = FALSE)
+
+animated_fig <- plot_ly(data_aggregated, x = ~avg_popularity, y = ~avg_danceability, color = ~count, text = ~genre, type = 'scatter', mode = 'markers', size = ~count, sizes = c(10, 100), frame = ~year) %>%
+  layout(title = list(text='', font=list(
+    color = "blue")), font = list(color = "blue", size = 15), 
+    xaxis = list(title = 'Average popularity', titlefont = list(size = 20)),
+    yaxis = list(title = 'Average danceability', titlefont = list(size = 20)),
+    plot_bgcolor = 'white', paper_bgcolor = 'white'
+  )
+animated_fig
